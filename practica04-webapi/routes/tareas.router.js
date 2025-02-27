3e29989d3728266db9e4841237f12b232c12d9b1
AlanM25 <2230233@upv.edu.mx>
2025-02-27 10:34:32 -0600

    clase 27/02

--- practica04-webapi/routes/tareas.router.js
+++ practica04-webapi/routes/tareas.router.js
@@ -8,4 +8,13 @@ router.get("/", async (req, res) => {
     res.json(await tareasService.obtenerTodas());
 });
 
+router.post("/", async (req, res) => {
+    const tareasService = new TareasService();
+    const tarea = req.body;
+    //TODO: Validar datos de la tarea
+    const idTarea = await tareasService.guardarNueva(tarea);
+    //Siempre debemos que responder algo al cliente
+    res.status(201).json({ message: "Tarea creada exitosamente", idTarea });
+});
+
 module.exports = router;
--- practica04-webapi/services/TareasService.js
+++ practica04-webapi/services/TareasService.js
@@ -1,15 +1,29 @@
-const mysql = require("mysql2/promise");
-const dbConfig = require("../dbConfig.json");
+const db = require("../dataAccess/db");
 
 class TareasService {
     
     constructor() {}
         
     async obtenerTodas() {
-        const db = await mysql.createConnection(dbConfig);
         const [rows] = await db.execute("SELECT * FROM tareas");
-        await db.end();
-        return rows;
+        return rows.map(r => { return {
+            id: r.id,
+            descripcion: r.descripcion,
+            fechaRegistro: r.fecha_registro.toLocaleString("es-MX"),
+            fechaCaduca: r.fecha_caduca?.toLocaleString("es-MX"),
+            concluido: r.concluido != 0
+        }
+        });
+    }
+    
+    async guardarNueva(tarea) {
+        const sql = 
+            "INSERT INTO tareas" +
+            " (descripcion, fecha_registro, fecha_caduca, concluido)" +
+            " VALUES (?, ?, ?, ?)";
+        const p = [tarea.descripcion, tarea.fechaRegistro, tarea.fechaCaduca, 0];
+        const [r] = await db.execute(sql, p);
+        return r.insertId;
     }
 }
 
